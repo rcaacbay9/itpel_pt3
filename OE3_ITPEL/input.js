809e796c9dfe46980081757fbf0f7c4e44ac45af
r.caacbay9 <r.caacbay9@gmail.com>
2021-09-24 16:34:40 +0800

    init

---
+++ OE3_ITPEL/input.js
@@ -0,0 +1,36 @@
+var ename = document.querySelector('#name');
+var hoursWork = document.querySelector('#hWork');
+var rateHour = document.querySelector('#rHour');
+var Calc = document.querySelector('#Calculate');
+var clear = document.querySelector('#reset')
+
+var hworkValue = hoursWork.value;
+
+var salary = hworkValue;
+const removed = () =>{
+    ename.value = '';
+    hoursWork.value =  '';
+    rateHour.value = '';
+}
+clear.addEventListener('click', removed);
+
+Calc.addEventListener('click', () => {
+    var nameValue = ename.value;
+    var hworkValue = hoursWork.value;
+    var rhourValue = rateHour.value;
+    var salary = hworkValue * rhourValue;
+    var tax = 0; 
+    if (salary > 50000){
+        tax = 0.25;
+    }
+    if (salary < 50000){
+        tax = 0.15;
+    }
+    var final = salary - (tax * salary);
+    console.log(final, nameValue, tax);
+    alert(
+    "Name: " + nameValue + "\n" +  
+    "Tax: " + tax * 100 + "% "+"\n" + 
+    "Gross Pay: " + final
+    );
+})
